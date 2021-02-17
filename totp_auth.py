52ec0f76cafeab9a9fc9dbd3c76556ff3efe98b9
arkoz <75336759+4rk0z@users.noreply.github.com>
2021-02-18 00:15:12 +0100

    Update totp_auth.py

--- totp_auth.py
+++ totp_auth.py
@@ -15,8 +15,7 @@ def hotp(secret, intervals_no):
 
 
 def totp(secret):
-    return hotp(secret, intervals_no=int(time.time()) //
-                30), str(round(30 - time.time() % 30)) + "s"
+    return hotp(secret, intervals_no=int(time.time()) // 30), str(round(30 - time.time() % 30)) + "s"
 
 
 secret = 'TESTABCDEFGHIJKL'
